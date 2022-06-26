d0b8f45c829aef491089495357a42f7b7cf12b59
Tubshinbat <tuvshinbat596@gmail.com>
2022-06-27 00:05:19 +0800

    port

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,15 +1,15 @@
 module.exports = {
   apps: [
     {
-      name: "naog-cms-api",
+      name: "jet-cms-api",
       script: "npm start",
       args: ["--color"],
       env: {
         NODE_ENV: "prod",
         SERVER_ENV: "prod",
         DEBUG: "server:*",
-        DEBUG_COLORS: true
-      }
-    }
-  ]
+        DEBUG_COLORS: true,
+      },
+    },
+  ],
 };
